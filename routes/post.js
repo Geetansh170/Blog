73ef5317f4cb14b5c1f3c452f751115377f4dd4b
geetansh170 <geetanshkumar170@gmail.com>
2020-04-08 19:10:59 +0530

    first commit

---
+++ routes/post.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+
+const Post = require("../models/Post");
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find().sort({
+      date: -1,
+    });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { title, content } = req.body;
+
+  try {
+    const newPost = new Post({
+      title,
+      content,
+    });
+
+    const post = await newPost.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(er.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: "Post not found" });
+
+    await Post.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+module.exports = router;
